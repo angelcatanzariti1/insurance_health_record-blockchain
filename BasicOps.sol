c844fda644b56e325927d3a79ced58ee7420ef58
Angel Catanzariti <angel@innship.com>
2022-01-22 08:21:53 -0300

    Factory and imported files

---
+++ BasicOps.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.4 <=0.8.11;
+pragma experimental ABIEncoderV2;
+import "./SafeMath.sol";
+import "./ERC20.sol";
+import "./BasicOps.sol";
+
+//Contract for the insurance company
+contract InsuranceFactory is BasicOps{
+    
+}
