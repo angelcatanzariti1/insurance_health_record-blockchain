c84b1a17f1ae089e6343852dc694043246ec4fb9
Angel Catanzariti <angelcatanzariti1@gmail.com>
2022-02-04 14:19:05 -0300

    structures, mappings & arrays

--- HealthInsuranceFactory.sol
+++ HealthInsuranceFactory.sol
@@ -11,19 +11,44 @@ contract HealthInsuranceFactory is BasicOperations{
     //instance of token contract
     ERC20Basic private token;
 
+    //structures
     constructor(){
         token = new ERC20Basic(100);
         Insurance = address(this);
         Carrier = msg.sender;
     }
 
+    struct client{
+        address ClientAddress;
+        bool ClientAuthorization;
+        address ContractAddress;
+    }
+
+    struct service{
+        string ServiceName;
+        uint ServiceTokenPrice;
+        bool ServiceStatus;
+    }
+
+    struct lab{
+        address LabContractAddress;
+        bool LabValidation;
+    }
+
     //addresses declaration
     address Insurance;
     address payable public Carrier;
 
-    address[] InsuredAddresses;
-
+    //mappings for clients, services and labs
+    mapping(address => client) public MappingClients;
+    mapping(string => service) public MappingServices;
+    mapping(address => lab) public MappingLabs;
     
+    //arrays for clients, services and labs
+    string[] private ServicesNames;
+    address[] LabsAddresses;
+    address[] ClientsAddresses;
+
 
 
 }
